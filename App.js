502d8712709ce33395c53bca44b7e51ee0d56555
abhivarma07 <abhijeetthemessi@gmail.com>
2021-12-09 23:13:47 +0530

    Handled user input and output

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ import {
   TextInput,
 } from "react-native"
 
+import SnackBar from "react-native-snackbar"
+
+
 const currencyPerRupee = {
   DOLLAR : 0.013,
   EURO : 0.012,
@@ -19,6 +22,7 @@ const currencyPerRupee = {
   YEN : 1.50,
   DINAR : 0.0040,
   RIYAL : 0.050,
+  RUBLE : 0.98,
 }
 
 const App = () => {
@@ -26,12 +30,30 @@ const App = () => {
   const [inputValue,setInput] = useState(0);
   const [result,setResult] = useState(0);
 
+  const buttonPressed = (currency) => {
+    if(!inputValue){
+      return SnackBar.show({
+        text: 'Please enter a numeric value',
+        backgroundColor : "#EA7773",
+        textColor : "#FFFFFF",
+      });
+    }
+
+    let result = parseFloat(inputValue) * currencyPerRupee[currency]
+    setResult(result.toFixed(4))
+  }
+
   return(
     <>
-    <ScrollView >
+    <ScrollView backgroundColor = "#1b262c"
+    keyboardShouldPersistTaps="handled"
+    contentInsetAdjustmentBehavior="automatic"
+    >
       <SafeAreaView style={styles.container}>
         <View style = {styles.resultContainer}>
-          <Text style = {styles.resultValue}>12.23</Text>
+          <Text style = {styles.resultValue}>
+            {result}
+          </Text>
         </View>
         <View style = {styles.inputContainer} >
           <TextInput
@@ -39,7 +61,20 @@ const App = () => {
           keyboardType="numeric"
           placeholder="Enter Value"
           placeholderTextColor="#c1c1c1"
+          value = {inputValue}
+          onChangeText={(inputValue) => setInput(inputValue)}
           ></TextInput>
+        </View>
+        <View style = {styles.convertButtonContainer}> 
+          {Object.keys(currencyPerRupee).map((currency) => (
+            <TouchableOpacity
+            key = {currency}
+            style = {styles.converterButton}
+            onPress={() => buttonPressed(currency)}
+            >
+              <Text style = {styles.converterText}>{currency}</Text>
+            </TouchableOpacity>
+          ))}
         </View>
       </SafeAreaView>
     </ScrollView>
@@ -92,6 +127,22 @@ const styles = StyleSheet.create({
     flexDirection : "row",
     flexWrap : "wrap",
     marginTop : 10 ,
+  } , 
+
+  converterButton : {
+    marginTop: 10 ,
+    alignItems : "center",
+    justifyContent : "center",
+    height : 100 ,
+    width : "33.3%",
+    borderColor : "#bbe1fa",
+    borderWidth : 2 ,
+    backgroundColor : "#0f4c75",
+    borderRadius : 10 ,
+  },
+
+  converterText : {
+    color : "#fff"
   }
 
 })
